a04c0bb3c1973770751c8aba68b5d2ae52e920db
hoslack <hoslackochieng@gmail.com>
2017-12-19 12:53:52 +0200

    creatd some routes and error messages
    relates #8

--- src/app.js
+++ src/app.js
@@ -6,9 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const exhbs = require('express-handlebars');
 const helpers = require('./views/helpers/index');
-
-const index = require('./routes/index');
-const users = require('./routes/users');
+const controllers = require('./controllers/index');
 
 const app = express();
 
@@ -34,8 +32,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(controllers);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
